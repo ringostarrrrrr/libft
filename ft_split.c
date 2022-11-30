b5a0d988512c6583c4ef3965de23dd8eb3544a32
ringostarrrrrr <104681365+ringostarrrrrr@users.noreply.github.com>
2022-11-30 22:18:40 +0900

    Add files via upload
    スプリットlibftTesterでオッケー
    割と自力、ナイス

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: naokifuse <naokifuse@student.42.fr>        +#+  +:+       +#+        */
+/*   By: naoki <naoki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 14:01:52 by naokifuse         #+#    #+#             */
-/*   Updated: 2022/11/27 22:46:13 by naokifuse        ###   ########.fr       */
+/*   Updated: 2022/11/30 22:15:49 by naoki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,37 +28,63 @@ size_t	my_countsplit(char const *s, char c)
 	return (cnt);
 }
 
-char	**ft_split(char const *s, char c)
+char	**my_free(char **ptr, size_t i)
 {
-	size_t	cntsplit;
-	char	**ptr;
+	size_t	j;
+
+	j = 0;
+	if (ptr[i] == NULL)
+	{
+		while (j < i)
+		{
+			free(ptr[j]);
+			j++;
+		}
+		return (NULL);
+	}
+	return (ptr);
+}
+
+char	**my_split(char **ptr, char const *s, char c, size_t cntsplit)
+{
 	size_t	i;
 	size_t	cnt_len;
 	char	*tmp;
 
-	if (!s)
-		return (NULL);
-	cntsplit = my_countsplit(s, c);
-	ptr = (char **)malloc((cntsplit + 1) * sizeof(char));
-	if (!ptr)
-		return (NULL);
 	i = 0;
 	cnt_len = 0;
-	while (*s)
+	while (i < cntsplit)
 	{
 		if (*s != c)
 		{
 			tmp = (char *)s;
-			while (*s && *s != c) 
+			while (*s && *s != c)
 			{
 				cnt_len++;
 				s++;
 			}
-			ptr[i++] = ft_substr(tmp, 0, cnt_len);
+			ptr[i] = ft_substr(tmp, 0, cnt_len);
+			my_free(ptr, i);
+			i++;
 			cnt_len = 0;
 		}
 		s++;
 	}
 	ptr[i] = NULL;
+	return (ptr);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	cntsplit;
+	char	**ptr;
+
+	if (!s)
+		return (NULL);
+	cntsplit = my_countsplit(s, c);
+	ptr = (char **)malloc((cntsplit + 1) * sizeof(char *));
+	if (!ptr)
+		return (NULL);
+	ptr = my_split(ptr, s, c, cntsplit);
 	return (ptr);
 }
