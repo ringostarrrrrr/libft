af8f6e22c91b80a8097b7b1e1594158308ac74ec
ringostarrrrrr <104681365+ringostarrrrrr@users.noreply.github.com>
2022-12-02 16:01:16 +0900

    Add files via upload
    part2終わった！strnbrはfree使えないの痛い

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: naoki <naoki@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/02 15:25:05 by naoki             #+#    #+#             */
+/*   Updated: 2022/12/02 15:55:37 by naoki            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int un;
+
+	if (n < 0)
+	{
+		un = (unsigned int)n * -1;
+		ft_putchar_fd('-', fd);
+	}
+	else
+		un = (unsigned int)n;
+	if (un >= 10)
+		ft_putnbr_fd(un / 10, fd);
+	ft_putchar_fd(un % 10 + '0', fd);
+}
