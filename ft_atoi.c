ae36ab2109111d9cf4ef129c368232bdb57fbf7a
ringostarrrrrr <104681365+ringostarrrrrr@users.noreply.github.com>
2022-12-10 18:28:12 +0900

    Add files via upload
    atoiを直した

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,21 +6,28 @@
 /*   By: naokifuse <naokifuse@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 22:26:57 by naokifuse         #+#    #+#             */
-/*   Updated: 2022/11/26 10:32:42 by naokifuse        ###   ########.fr       */
+/*   Updated: 2022/12/10 18:25:13 by naokifuse        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
+int	overflow(int isminus)
+{
+	if (isminus == 1)
+		return ((int)LONG_MAX);
+	else
+		return ((int)LONG_MIN);
+}
+
 int	ft_atoi(const char *s)
 {
-	int	isminus;
-	int	result;
+	int		isminus;
+	long	result;
 
 	isminus = 1;
-	while (*s == ' ' || *s == '\t' || *s == '\v'
-		|| *s == '\f' || *s == '\r' || *s == '\n')
+	while ((9 <= *s && *s <= 13) || *s == 32)
 		s++;
 	if (*s == '+' || *s == '-')
 	{
@@ -31,8 +38,12 @@ int	ft_atoi(const char *s)
 	result = 0;
 	while ('0' <= *s && *s <= '9')
 	{
+		if (result > LONG_MAX / 10)
+			return (overflow(isminus));
+		if (result == LONG_MAX / 10 && LONG_MAX % 10 < *s - '0')
+			return (overflow(isminus));
 		result *= 10;
-		result += (*s - 48);
+		result += (*s - '0');
 		s++;
 	}
 	result *= isminus;
